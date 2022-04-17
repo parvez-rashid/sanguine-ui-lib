d00fc00593a12f0e4f6b25c6da26d61637913674
parvez-rashid <84724590+parvez-rashid@users.noreply.github.com>
2022-04-17 14:01:20 +0530

    Feat: modal

---
+++ components/modal/modal.js
@@ -0,0 +1,26 @@
+const modal = document.querySelector(".modal");
+const btn = document.querySelector("#modal-open");
+const approve = document.querySelector("#modal-approve");
+const span = document.querySelector("#modal-close");
+
+btn.addEventListener("click", () => {
+  console.log("clicked");
+  modal.style.display = "block";
+});
+
+span.addEventListener("click", () => {
+  console.log("clicked");
+  modal.style.display = "none";
+});
+
+approve.addEventListener("click", () => {
+  console.log("clicked");
+  modal.style.display = "none";
+});
+
+// When the user clicks anywhere outside of the modal, close it
+window.addEventListener("click", (event) => {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
